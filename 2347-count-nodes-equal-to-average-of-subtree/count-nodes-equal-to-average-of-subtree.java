029a8297368672955ca09d5fafa56782e694b9a9
SAYAN GHOSH <sayan.23bce11674@vitbhopal.ac.in>
2026-09-10 17:15:37 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (37.15%) - LeetSync

---
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int matchingNodes = 0;
+
+    public int averageOfSubtree(TreeNode root) {
+        matchingNodes = 0;
+        dfs(root);
+        return matchingNodes;
+    }
+
+    // Returns an array of size 2: [subtreeSum, nodeCount]
+    private int[] dfs(TreeNode node) {
+        if (node == null) {
+            return new int[]{0, 0};
+        }
+
+        int[] left = dfs(node.left);
+        int[] right = dfs(node.right);
+
+        int sum = left[0] + right[0] + node.val;
+        int count = left[1] + right[1] + 1;
+
+        if (sum / count == node.val) {
+            matchingNodes++;
+        }
+
+        return new int[]{sum, count};
+    }
+}
