1b8fbf0cd753f59e43827faf534ba1962eef38dd
SAYAN GHOSH <sayan.23bce11674@vitbhopal.ac.in>
2026-09-11 12:28:06 +0530

    Time: 0 ms (100.00%) | Memory: 28.6 MB (93.75%) - LeetSync

---
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+        vector<int> freq(10, 0);
+        for (int d : digits) {
+            freq[d]++;
+        }
+
+        int count = 0;
+        for (int num = 100; num < 1000; num += 2) {
+            int d1 = num / 100;
+            int d2 = (num / 10) % 10;
+            int d3 = num % 10;
+
+            freq[d1]--;
+            freq[d2]--;
+            freq[d3]--;
+
+            if (freq[d1] >= 0 && freq[d2] >= 0 && freq[d3] >= 0) {
+                count++;
+            }
+
+            freq[d1]++;
+            freq[d2]++;
+            freq[d3]++;
+        }
+
+        return count;
+    }
+};
