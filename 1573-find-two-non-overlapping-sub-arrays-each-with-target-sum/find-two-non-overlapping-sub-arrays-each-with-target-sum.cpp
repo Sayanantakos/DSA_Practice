d1f20828d6b776b4e8e2a457bc182390d243605a
SAYAN GHOSH <sayan.23bce11674@vitbhopal.ac.in>
2026-09-17 23:30:03 +0530

    Time: 4 ms (88.92%) | Memory: 88.9 MB (67.53%) - LeetSync

---
+++ 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int minSumOfLengths(vector<int>& arr, int target) {
+        int n = arr.size();
+        const int INF = 1e9;
+        vector<int> best(n, INF);
+
+        int ans = INF;
+        int left = 0;
+        int sum = 0;
+
+        for (int right = 0; right < n; right++) {
+            sum += arr[right];
+
+            while (sum > target) {
+                sum -= arr[left++];
+            }
+
+            if (sum == target) {
+                int len = right - left + 1;
+                if (left > 0 && best[left - 1] != INF) {
+                    ans = min(ans, len + best[left - 1]);
+                }
+                best[right] = min(right > 0 ? best[right - 1] : INF, len);
+            } else {
+                best[right] = right > 0 ? best[right - 1] : INF;
+            }
+        }
+
+        return ans >= INF ? -1 : ans;
+    }
+};
