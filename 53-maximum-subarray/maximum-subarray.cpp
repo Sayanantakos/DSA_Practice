8e5d6337c06b0725f6ca3194189f895fbd45620d
SAYAN GHOSH <sayan.23bce11674@vitbhopal.ac.in>
2026-09-23 21:54:44 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (80.99%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currentSum = nums[0];
+        int maxSoFar = nums[0];
+
+        for (size_t i = 1; i < nums.size(); ++i) {
+            currentSum = max(nums[i], currentSum + nums[i]);
+            maxSoFar = max(maxSoFar, currentSum);
+        }
+
+        return maxSoFar;
+    }
+};
